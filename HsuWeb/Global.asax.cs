9b5bf87b0c31764a8290b7395164f5a617035ba6
EK3680 <EK3680@SKEK3680>
2017-03-30 18:41:46 +0800

    加入專案檔案。

---
+++ HsuWeb/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using HsuWeb.APP_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace HsuWeb
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ViewEngineConfig.RegisterViewEngines(ViewEngines.Engines);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HsuWeb/Scripts/vue/Component.js
@@ -0,0 +1,52 @@
+﻿/*Component Data*/
+var bus = new Vue();
+
+/* Header */
+Vue.component('app-header', {
+    template: '<div>Header!<input type=text id = ss  v-model = "message" v-on:blur="doclick()">'+
+                '<p><b>shooooow: {{message}}</b></p>'+
+                '</div>',
+    data:function(){
+        return { message: 'hello' }
+    },
+    methods:{
+        doclick: function () {
+            bus.$emit('id-selected', this.message);
+        }
+    }
+})
+/* Footer */
+Vue.component('app-footer', {
+    template: '<div>footer!</div>'
+})
+/* Content-Intro*/
+Vue.component('app-intro', {
+    template: '<div>Intro</div>',
+        created() {
+        bus.$on('id-selected', function (id) {             
+            alert(id+"--intro")
+        })
+    }
+});
+
+/* Content-Artical */
+Vue.component('app-artical', {
+    template: '<div>artical <label>:: {{getval}}</label> </div>',
+    data:function(){
+        return{getval:'未改前'}
+    },
+    
+    created() {
+        bus.$on('id-selected', function (id) {
+            this.getval = id;
+            alert(this.getval+"--artical")
+        })
+    }
+       
+    
+});
+
+/* Content-Contact */
+Vue.component('app-contact', {
+    template: '<div>Contact</div>'
+});
